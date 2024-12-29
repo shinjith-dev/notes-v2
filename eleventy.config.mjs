beb6b7564fc5fb814c02139da418dc983ecfb2d7
shinjith-dev <shinjithpallikkara@gmail.com>
2024-12-29 22:27:49 +0530

    feat: add link to pages
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,12 +1,28 @@
 import 'dotenv/config'
+import fs from 'fs';
 
 console.log(process.env)
 
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("bundle.js");
-  eleventyConfig.addPassthroughCopy("assets/fonts/Inter.ttf");
-  eleventyConfig.addPassthroughCopy("assets/fonts/Inconsolata.ttf");
+  eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy({ admin: process.env.ADMIN_PATH });
+
+  // eleventyConfig.addCollection("recentlyModified", function(collectionApi) {
+  //   return collectionApi.getAll()
+  //     .filter(item => item.inputPath.endsWith(".html")) // Ensure it's a content file
+  //     .map(item => {
+  //       // Retrieve the file's modification date
+  //       const stats = fs.statSync(item.inputPath);
+  //       return {
+  //         url: item.url,
+  //         title: item.data.title || "Untitled", // Fallback title if not defined
+  //         modifiedDate: stats.mtime // Add modification date
+  //       };
+  //     })
+  //     .sort((a, b) => b.modifiedDate - a.modifiedDate) // Sort descending by modification date
+  //     .slice(0, 10); // Limit to 10 entries
+  // });
 };
 
