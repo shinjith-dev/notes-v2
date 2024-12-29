1650dfdafdbf6b97453fc9661cfb240f11f834a8
shinjith-dev <shinjithpallikkara@gmail.com>
2024-12-29 20:06:22 +0530

    feat: complete basic setup
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,5 +1,8 @@
 
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
+  eleventyConfig.addPassthroughCopy("bundle.js");
+  eleventyConfig.addPassthroughCopy("assets/fonts/Inter.ttf");
+  eleventyConfig.addPassthroughCopy("assets/fonts/Inconsolata.ttf");
 };
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./**/*.{html,js}"],
+  darkMode: "selector",
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: ["Inter", "serif"],
+      display: ['"Oleo Script"', "cursive"],
+      mono: ['"Inconsolata"', "monospace"],
+    },
+    extend: {
+      colors: {
+        primary: "#425f5b",
+        brand: "#ee6c4d",
+        light: {
+          100: "#425f5b",
+          200: "#556f6c",
+          300: "#69807d",
+          400: "#7d918e",
+          500: "#92a3a0",
+          600: "#a7b5b2",
+        },
+        dark: {
+          100: "#171919",
+          200: "#2c2e2e",
+          300: "#434444",
+          400: "#5b5c5c",
+          500: "#747575",
+          600: "#8e8f8f",
+        },
+      },
+    },
   },
   plugins: [],
 };
