6e5563d243cddaeb6b38546540c49a9fb393062a
shinjith-dev <shinjithpallikkara@gmail.com>
2025-01-03 02:09:37 +0530

    feat: add recently modfied list
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+// import fs
 
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
@@ -6,20 +7,18 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy({ admin: process.env.ADMIN_PATH });
 
-  // eleventyConfig.addCollection("recentlyModified", function(collectionApi) {
-  //   return collectionApi.getAll()
-  //     .filter(item => item.inputPath.endsWith(".html")) // Ensure it's a content file
-  //     .map(item => {
-  //       // Retrieve the file's modification date
-  //       const stats = fs.statSync(item.inputPath);
-  //       return {
-  //         url: item.url,
-  //         title: item.data.title || "Untitled", // Fallback title if not defined
-  //         modifiedDate: stats.mtime // Add modification date
-  //       };
-  //     })
-  //     .sort((a, b) => b.modifiedDate - a.modifiedDate) // Sort descending by modification date
-  //     .slice(0, 10); // Limit to 10 entries
-  // });
+  eleventyConfig.addCollection("recentlyModified", (collectionApi) =>
+    collectionApi.getAll()
+      .filter(item => item.outputPath.endsWith(".html"))
+      .map(item => {
+        return {
+          url: item.url,
+          title: item.data.title || "Untitled",
+          modifiedDate: item.date
+        };
+      })
+      .sort((a, b) => b.modifiedDate - a.modifiedDate)
+      .slice(0, 10)
+  );
 };
 
