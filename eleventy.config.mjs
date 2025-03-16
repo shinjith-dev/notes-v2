296152ed734e2b57c902941318f1f42b0aca4693
shinjith-dev <shinjithpallikkara@gmail.com>
2025-03-16 15:46:15 +0530

    feat: add sitemap and robots.txt
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,9 +1,11 @@
 import 'dotenv/config'
+import sitemap from "@quasibit/eleventy-plugin-sitemap"
 
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("bundle.js");
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy({ admin: process.env.ADMIN_PATH });
 
   eleventyConfig.addCollection("recentlyModified", (collectionApi) =>
@@ -19,6 +21,12 @@ export default function(eleventyConfig) {
       .sort((a, b) => b.modifiedDate - a.modifiedDate)
       .slice(0, 10)
   );
+
+   eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://notes.shinjith.dev",
+    },
+  });
 };
 
 export const config = {
